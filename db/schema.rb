6c07b8605c180a734de4a1f945115f3999a939da
ganeshran <ganesh.ranganathan.85@gmail.com>
2015-04-09 14:59:06 +0530

    Changes in dockerfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310171221) do
+ActiveRecord::Schema.define(version: 20150409002105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "imagefiles", force: true do |t|
+    t.string   "name",       limit: nil
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "organizations", force: true do |t|
     t.string "name"
